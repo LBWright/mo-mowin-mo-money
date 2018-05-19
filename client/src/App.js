b7e5ad4f123eb600dafeeb2c3ac79ebe4666c7df
Christopher Fudala <cfudala82@me.com>
2018-05-19 18:54:02 -0500

    invoice

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Invoice from './components/Invoice/Invoice';
 
 class App extends Component {
   render() {
