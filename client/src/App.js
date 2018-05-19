a86de6ea4c205cb922d04d1df6f77737f2a0bb22
Christopher Fudala <cfudala82@me.com>
2018-05-19 18:56:58 -0500

    invoice

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-import Invoice from './components/Invoice/Invoice';
-=======
 import { Route, Switch, Redirect } from 'react-router-dom';
 
->>>>>>> 5dcafba2f5244c869c33e93b325585542767004a
 
 class App extends Component {
   render() {
