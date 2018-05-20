35a1341883591e9bd3c34f96becd5a4e5b8aba12
kristenbaldwin <kristen.baldwin22@gmail.com>
2018-05-20 12:52:12 -0500

    Filled in login jumbotron and added LinkContainers to react-bootstrap nav to keep pages from refreshing on click

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar, Nav, NavItem, NavDropdown, MenuItem, FormControl, FormGroup, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import './Header.css';
 
 class Header extends Component {
@@ -8,29 +9,41 @@ class Header extends Component {
             <Navbar className="navbar">
               <Navbar.Header>
                 <Navbar.Brand>
-                  <a className="navbar-name" href="/"><img className="logo" src= {require("./logo.jpg")} alt='logo' /></a>
+                  <LinkContainer to="/" className="navbar-name">
+                    <a className="navbar-name"><img className="logo" src= {require("./logo.jpg")} alt='logo' /></a>
+                  </LinkContainer>
                 </Navbar.Brand>
               </Navbar.Header>
               <Nav pullRight>
-                <NavItem eventKey={1} href="/All">
-                  All Lots
-                </NavItem>
-                <NavItem eventKey={2} href="/Login">
-                  Login
-                </NavItem>
-                <NavItem eventKey={3} href="/Register">
-                  Register
-                </NavItem>
-                <NavItem eventKey={3} href="/Invoice">
-                  Invoice
-                </NavItem>
+                <LinkContainer to="/All">
+                  <NavItem eventKey={1}>
+                    All Lots
+                  </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/Login">
+                  <NavItem eventKey={2}>
+                    Login
+                  </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/Register">
+                  <NavItem eventKey={3}>
+                    Register
+                  </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/Invoice">
+                  <NavItem eventKey={3}>
+                    Invoice
+                  </NavItem>
+                </LinkContainer>
               </Nav>
               <Navbar.Form pullRight>
                 <FormGroup>
                   <FormControl type="text" placeholder="Search Lots" />
                 </FormGroup>{' '}
                 {/* Made this into a link to the single lot for the demo AKW */}
-                <a href="/lot"><Button type="submit">Search</Button></a>
+                <LinkContainer to="/lot">
+                  <Button type="submit">Search</Button>
+                </LinkContainer>
               </Navbar.Form>
             </Navbar>
         );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,10 +6,10 @@ class AllLots extends Component {
     render() {
         return (
             <Grid>
-                <div class="jumbotron jumbotron-fluid">
-                    <div class="container">
-                   <h1 class="display-4">Fluid jumbotron</h1>
-                   <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                <div className="jumbotron jumbotron-fluid">
+                  <div class="container">
+                    <h1 className="display-4">Neighborhood Mow-Down Program</h1>
+                    {/* <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p> */}
                  </div>
                 </div>
                 <Row>
@@ -19,23 +19,23 @@ class AllLots extends Component {
                       <div class="card-body">
                         <h5 class="card-title">123 Talshire</h5>
                         <p class="card-text">This yard needs mowed.</p>
-                        <a href="#" class="btn btn-primary btn-all">Go somewhere</a>
+                        <a href="#" class="btn btn-primary btn-all">View Lot</a>
                       </div>
                     </div>
                   <div class="card lotcard">
                     <img class="card-img-top lotimage" src={require("../../images/overgrown1.jpg")} alt="Card image cap" />
                     <div class="card-body">
                       <h5 class="card-title">777 Boeing</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary btn-all">Go somewhere</a>
+                      <p class="card-text">This yard needs mowed.</p>
+                      <a href="#" class="btn btn-primary btn-all">View Lot</a>
                     </div>
                   </div>
                   <div class="card">
                     <img class="card-img-top lotimage" src={require("../../images/overgrown2.jpeg")} alt="Card image cap" />
                     <div class="card-body">
-                      <h5 class="card-title">Card title</h5>
-                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                      <a href="#" class="btn btn-primary btn-all">Go somewhere</a>
+                      <h5 class="card-title">151 Georgia</h5>
+                      <p class="card-text">This yard needs mowed.</p>
+                      <a href="#" class="btn btn-primary btn-all">View Lot</a>
                     </div>
                   </div>
                   </Col>
