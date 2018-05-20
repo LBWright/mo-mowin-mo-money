1f3e89cd181780b28b2051af2ef865db0011e793
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-20 15:07:48 +0000

    list all update

--- client/src/components/AllLots/AllLots.js
+++ client/src/components/AllLots/AllLots.js
@@ -12,9 +12,9 @@ class AllLots extends Component {
                     </Col>
                     <Col md={8} className="lotInfo">
                         {/* <LotInfo /> */}
-                        <p><span className="bold">Address: </span>151 Georgia St, Houston, TX 77029</p>
+                        <p><span className="bold">Address: </span>4910 Avenue I, Houston, TX 77029</p>
                         <p><span className="bold">Status: </span><b>Active</b></p>
-                        <p><span className="bold">Super Neighborhood: </span>Clinton Park Tri-Community</p>
+                        <p><span className="bold">Super Neighborhood: </span>Second Ward</p>
                     </Col>
                 </Row>
                 <Row className="pageHeader">
@@ -34,9 +34,31 @@ class AllLots extends Component {
                     </Col>
                     <Col md={8} className="lotInfo">
                         {/* <LotInfo /> */}
-                        <p><span className="bold">Address: </span>151 Georgia St, Houston, TX 77029</p>
+                        <p><span className="bold">Address: </span>4510 Siegel Rd, Houston, TX 77029</p>
                         <p><span className="bold">Status: </span>Active</p>
-                        <p><span className="bold">Super Neighborhood: </span>Clinton Park Tri-Community</p>
+                        <p><span className="bold">Super Neighborhood: </span>Near Northside</p>
+                    </Col>
+                </Row>
+                <Row className="pageHeader">
+                    <Col md={4}>
+                        <img src= {require("../../images/overgrown4.png")} alt='lot' />
+                    </Col>
+                    <Col md={8} className="lotInfo">
+                        {/* <LotInfo /> */}
+                        <p><span className="bold">Address: </span>5926 High Star Dr, Houston, TX 77029</p>
+                        <p><span className="bold">Status: </span>Active</p>
+                        <p><span className="bold">Super Neighborhood: </span>Gulfton</p>
+                    </Col>
+                </Row>
+                <Row className="pageHeader">
+                    <Col md={4}>
+                        <img src= {require("../../images/overgrown5.png")} alt='lot' />
+                    </Col>
+                    <Col md={8} className="lotInfo">
+                        {/* <LotInfo /> */}
+                        <p><span className="bold">Address: </span>1620 Bland St, Houston, TX 77029</p>
+                        <p><span className="bold">Status: </span>Active</p>
+                        <p><span className="bold">Super Neighborhood: </span>Acres Home</p>
                     </Col>
                 </Row>
             </Grid>
