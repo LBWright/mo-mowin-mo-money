68a16d9926cf116828f68110b5e7ba3e5a9c6fc0
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-05-20 01:03:09 +0000

    registration page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header'
+import Register from './components/Register/Register'
 
 
 class App extends Component {
   render() {
     return (
+      <div>
       <Header />
+
+      </div>
     );
   }
 }
